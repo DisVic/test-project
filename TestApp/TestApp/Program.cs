72ea5682900f827daeea22adafa82948451cd300
KroN136 <kron2002a@yandex.ru>
2023-11-12 15:36:49 +0300

    Cloned project from another repo

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("This is a new feature.");
+            Console.WriteLine("An addition to the first feature.");
+
+            Console.WriteLine("Incoming the second feature loop!");
+
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine("This is the second feature.");
+        }
+    }
+}
